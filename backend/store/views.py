87e7f473532a4c1e6635a984203e0ec7a303f6d0
kirtiranjan26 <ranjankirti26@gmail.com>
2026-01-11 22:54:00 +0530

    frontend and backend connected

--- backend/store/views.py
+++ backend/store/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse as jsonResponse
 
+
+def home(request):
+    data={
+        'message':'Welcome to Ecommerce Store'
+    }
+    return jsonResponse(data)
 # Create your views here.
